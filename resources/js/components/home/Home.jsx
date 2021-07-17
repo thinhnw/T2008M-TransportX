453c120893436d973bb88751d075f41594000f23
thinhnw <thinhnv0412@gmail.com>
2021-07-17 20:52:22 +0700

    Init react app

---
+++ resources/js/components/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+function Example() {
+	return (
+    <div className="container-fluid">
+
+    </div>
+	);
+}
