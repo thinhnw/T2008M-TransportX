7188c27af1d46e12d006561c7cd5ef0b0a17e42a
Thanh Chiến <70896118+jackychun2002@users.noreply.github.com>
2021-07-23 09:54:49 +0700

    commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -94,13 +94,13 @@
                             @csrf
 
                             <div>
-                                <x-jet-label for="email" value="{{ __('Email') }}" />
-                                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                                <x-jet-label for="email" class="text-left" />
+                                <x-jet-input id="email" class="block mt-1 w-full" type="email" style="padding:14px 28px;width: 100%;height: 50px;border: 20px;background: #fff0f2;outline: none" name="email" :value="old('email')" required autofocus placeholder="Email"/>
                             </div>
 
                             <div class="mt-4">
-                                <x-jet-label for="password" value="{{ __('Password') }}" />
-                                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+                                <x-jet-label for="password" class="text-left" />
+                                <x-jet-input id="password" class="block mt-1 w-full" type="password" style="padding:14px 28px;width: 100%;height: 50px;border: 20px;background: #fff0f2;outline: none" name="password" required autocomplete="current-password" placeholder="Password" />
                             </div>
 
                             <div class="block mt-4">
